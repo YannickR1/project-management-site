a3134cce924a72ecae5399958093f0ee72929833
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:21:33 +0200

    navbar layout

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+
 // styles & images
-import './Navbar.css'
-import Temple from '../../assets/temple.svg'
+import "./Navbar.css";
+import Temple from "../../assets/temple.svg";
 
 export default function Navbar() {
   return (
-    <div></div>
-  )
+    <div>
+      <ul>
+        <li className="logo">
+          <img src={Temple} alt="igloo logo" />
+          <span>The Igloo</span>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
