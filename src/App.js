8d2e9e10ea6f73642526d07ba6545013cd42dbc3
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:09:48 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// styles
+import "./App.css";
+
+// pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/project/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/Signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
