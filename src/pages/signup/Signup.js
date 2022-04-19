6a9288cfcc84c029b3b177cd729b4c5e92276529
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:57:45 +0200

    Signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,48 @@
-
 // styles
-import './Signup.css'
+import { useState } from "react";
+import "./Signup.css";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>Signup</div>
-  )
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display name:</span>
+        <input
+          type="text"
+          required
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile picture:</span>
+        <input type="file" />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
