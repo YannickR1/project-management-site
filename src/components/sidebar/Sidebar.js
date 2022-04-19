5ce24181e8c97252dd5cc4a6f35c04f56f847412
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:41:33 +0200

    icon imports

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,7 @@
-// styles
+// styles & images
 import './Sidebar.css'
+import DashboardIcon from '../../assets/dasboard_icon.svg'
+import AddIcon from '../../assets/add_icon.svg'
 
 export default function Sidebar() {
   return (
