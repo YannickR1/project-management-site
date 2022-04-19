3f4a8e811b76865f181af0a6112bde0f24c47f0f
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:12:37 +0200

    import logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,9 @@
+// styles & images
 import './Navbar.css'
+import Temple from '../../assets/temple.svg'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div></div>
   )
 }
