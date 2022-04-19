12df86007d251656ac5ee7d1e7ab80621817824a
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:46:07 +0200

    sidebar layout + basic content

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,35 @@
+import { NavLink } from "react-router-dom";
+
 // styles & images
-import './Sidebar.css'
-import DashboardIcon from '../../assets/dasboard_icon.svg'
-import AddIcon from '../../assets/add_icon.svg'
+import "./Sidebar.css";
+import DashboardIcon from "../../assets/dasboard_icon.svg";
+import AddIcon from "../../assets/add_icon.svg";
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          {/* avatar and username here later */}
+          <p>Hey user</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to="/">
+                <img src={DashboardIcon} alt="dashboard link icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="create link icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
