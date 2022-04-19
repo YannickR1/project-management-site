319e8e2809269a2184e5c3fec27f2c92ece2fe12
YannickR1 <yannickraats@gmail.com>
2022-04-19 21:11:20 +0200

    signup capitalization

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <Route path="/login">
               <Login />
             </Route>
-            <Route path="/Signup">
+            <Route path="/signup">
               <Signup />
             </Route>
           </Switch>
