322261dab7df2ebe2aeb5253ae432ef2cfcf0227
YannickR1 <yannickraats@gmail.com>
2022-04-19 22:38:33 +0200

    signup form log on submit

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,46 @@
-// styles
+// styles & images
 import { useState } from "react";
 import "./Signup.css";
+import DefaultPicture from "../../assets/defaultpf.png";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      // console.log('hi')
+      // selected = DefaultPicture;
+      // setThumbnail(selected)
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 500000) {
+      setThumbnailError("Image file size must be less than 500kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>Email:</span>
@@ -40,7 +71,8 @@ export default function Signup() {
       </label>
       <label>
         <span>Profile picture:</span>
-        <input type="file" />
+        <input required type="file" onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <button className="btn">Sign up</button>
     </form>
