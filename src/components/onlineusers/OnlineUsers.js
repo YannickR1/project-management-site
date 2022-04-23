6b09998549e09697c047d421fef525245619cda9
YannickR1 <yannickraats@gmail.com>
2022-04-23 18:15:59 +0200

    online user list

---
+++ src/components/onlineusers/OnlineUsers.js
@@ -0,0 +1,21 @@
+import { useCollection } from "../../hooks/useCollection";
+import Avatar from "../avatar/Avatar";
+
+// styles
+import "./OnlineUsers.css";
+
+export default function OnlineUsers() {
+  const { error, documents } = useCollection("users");
+  return (
+    <div className="user-list">
+      <h2>All Users</h2>
+      {error && <div>{error}</div>}
+      {documents && documents.map(user => (
+        <div key={user.id}>
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL}/>
+        </div>
+      ))}
+    </div>
+  );
+}
