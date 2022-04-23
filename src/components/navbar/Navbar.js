a02b3ca40ef9f214499daa989dc4697b3775faaa
YannickR1 <yannickraats@gmail.com>
2022-04-23 17:55:35 +0200

    conditional nav bar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-import { useLogout } from '../../hooks/useLogout'
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // styles & images
 import "./Navbar.css";
 import Igloo from "../../assets/igloo.png";
 
 export default function Navbar() {
-  const { logout, isPending } = useLogout()
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
   return (
     <div className="navbar">
       <ul>
@@ -15,16 +17,33 @@ export default function Navbar() {
           <span>The Igloo</span>
         </li>
 
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-          {isPending && <button className="btn" disabled>Logging out</button>}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>
+              {!isPending && (
+                <button className="btn" onClick={logout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" disabled>
+                  Logging out
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
