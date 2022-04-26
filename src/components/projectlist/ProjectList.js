cadd4358e0fbbbf71ebd5302bba95cd12b09aa01
YannickR1 <yannickraats@gmail.com>
2022-04-26 16:25:20 +0200

    project list component

--- src/components/projectlist/ProjectList.js
+++ src/components/projectlist/ProjectList.js
@@ -1,8 +1,14 @@
 // styles
-import './ProjectList.css'
+import "./ProjectList.css";
 
-export default function ProjectList() {
+export default function ProjectList({ projects }) {
   return (
-    <div>ProjectList</div>
-  )
+    <div>
+      {projects.length === 0 && <p>No projects yet!</p>}
+
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  );
 }
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import ProjectList from "../../components/projectlist/ProjectList";
 import { useCollection } from "../../hooks/useCollection";
 
 // styles
@@ -9,9 +10,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-      {documents && documents.map(doc =>(
-        <p key={doc.id}>{doc.name}</p>
-      ))}
+      {documents && <ProjectList projects={documents}/>}
     </div>
   );
 }
