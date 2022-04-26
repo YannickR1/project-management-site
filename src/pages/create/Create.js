913e0ae730cbdeb795544a7ab2b5320e8f28529d
YannickR1 <yannickraats@gmail.com>
2022-04-26 16:29:23 +0200

    create fix

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,118 +1,116 @@
-import { useEffect, useState } from "react";
-import Select from "react-select";
-import { useCollection } from "../../hooks/useCollection";
-import { timestamp } from "../../firebase/config";
-import { useAuthContext } from "../../hooks/useAuthContext";
-import { useFirestore } from "../../hooks/useFirestore";
-import { useHistory } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useCollection } from '../../hooks/useCollection'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { timestamp } from '../../firebase/config'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useHistory } from 'react-router'
+import Select from 'react-select'
 
 // styles
-import "./Create.css";
+import './Create.css'
 
 const categories = [
-  { value: "development", label: "Development" },
-  { value: "design", label: "Design" },
-  { value: "sales", label: "Sales" },
-  { value: "marketing", label: "Marketing" },
-];
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
 
 export default function Create() {
-  const { documents } = useCollection("users");
-  const [users, setUsers] = useState([]);
-  const { user } = useAuthContext();
-  const { addDocument, response } = useFirestore('projects');
-  const { history } = useHistory();
+  const history = useHistory()
+  const { addDocument, response } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const { documents } = useCollection('users')
+  const [users, setUsers] = useState([])
 
   // form field values
-  const [name, setName] = useState("");
-  const [details, setDetails] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const [category, setCategory] = useState("");
-  const [assignedUsers, setAssignedUsers] = useState([]);
-  const [formError, setFormError] = useState(null);
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
+  // create user values for react-select
   useEffect(() => {
-    if (documents) {
-      const options = documents.map((user) => {
-        return { value: user, label: user.displayName };
-      });
-      setUsers(options);
+    if(documents) {
+      setUsers(documents.map(user => {
+        return { value: {...user, id: user.id}, label: user.displayName }
+      }))
     }
-  }, [documents]);
+  }, [documents])
 
-  const  handleSubmit = async (e) => {
-    e.preventDefault();
-    setFormError(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFormError(null)
 
     if (!category) {
-      setFormError("Please select a category for this project");
-      return;
+      setFormError('Please select a project category.')
+      return
     }
     if (assignedUsers.length < 1) {
-      setFormError("Please assign at least 1 user to this project");
-      return;
+      setFormError('Please assign the project to at least 1 user')
+      return
     }
 
-    const createdBy = {
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      id: user.uid,
-    };
-
-    const assignedUsersList = assignedUsers.map((u) => {
-      return {
-        displayName: u.value.displayName,
+    const assignedUsersList = assignedUsers.map(u => {
+      return { 
+        displayName: u.value.displayName, 
         photoURL: u.value.photoURL,
-        id: u.value.id,
-      };
-    });
+        id: u.value.id
+      }
+    })
+    const createdBy = { 
+      displayName: user.displayName, 
+      photoURL: user.photoURL,
+      id: user.uid
+    }
 
     const project = {
       name,
       details,
-      dueDate: timestamp.fromDate(new Date(dueDate)),
-      category: category.value,
-      comments: [],
+      assignedUsersList, 
       createdBy,
-      assignedUsersList,
-    };
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: []
+    }
 
-    await addDocument(project);
-    if (!response.error){
+    await addDocument(project)
+    if (!response.error) {
       history.push('/')
     }
-  };
+  }
 
   return (
     <div className="create-form">
-      <h2 className="page-title">Create a new project</h2>
+      <h2 className="page-title">Create a new Project</h2>
       <form onSubmit={handleSubmit}>
         <label>
           <span>Project name:</span>
           <input
-            required
-            type="text"
+            required 
+            type="text" 
             onChange={(e) => setName(e.target.value)}
             value={name}
           />
         </label>
         <label>
-          <span>Project details:</span>
-          <textarea
+          <span>Project Details:</span>
+          <textarea 
             required
-            type="text"
             onChange={(e) => setDetails(e.target.value)}
-            value={details}
+            value={details} 
           ></textarea>
         </label>
         <label>
-          <span>Due date:</span>
+          <span>Set due date:</span>
           <input
-            required
-            type="date"
-            onChange={(e) => setDueDate(e.target.value)}
+            required 
+            type="date" 
+            onChange={(e) => setDueDate(e.target.value)} 
             value={dueDate}
-          ></input>
+          />
         </label>
         <label>
           <span>Project category:</span>
@@ -129,9 +127,11 @@ export default function Create() {
             isMulti
           />
         </label>
+
         <button className="btn">Add Project</button>
+
         {formError && <p className="error">{formError}</p>}
       </form>
     </div>
-  );
+  )
 }
