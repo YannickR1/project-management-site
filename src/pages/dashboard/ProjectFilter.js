d54cf489859eca7f5aa08eee33fda733e1a09763
YannickR1 <yannickraats@gmail.com>
2022-05-08 13:01:16 +0200

    project filter component

--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -1,22 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+const filterList = [
+  "all",
+  "mine",
+  "development",
+  "design",
+  "marketing",
+  "sales",
+];
 
 export default function ProjectFilter() {
-    const [currentFilter, setCurrentFilter] = useState('all')
+  const [currentFilter, setCurrentFilter] = useState("all");
 
-    const handleClick = (newFilter) => {
-        setCurrentFilter(newFilter)
-    }
-    return (
-        <div className='project-filter'>
-            <nav>
-                {filterList.map((filter) => (
-                    <button key={filter} onClick={() => handleClick(filter)}>
-                        {filter}
-                    </button>
-                ))}
-            </nav>
-        </div>
-    )
+  const handleClick = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+  return (
+    <div className="project-filter">
+      <nav>
+      <p>Filter by:</p>
+        {filterList.map((filter) => (
+          <button
+            key={filter}
+            onClick={() => handleClick(filter)}
+            className={currentFilter === filter ? "active" : ""}
+          >
+            {filter}
+          </button>
+        ))}
+      </nav>
+    </div>
+  );
 }
